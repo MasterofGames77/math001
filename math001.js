6bb9dbdb34f9ce399753909223ca77da5763362e
MasterofGames77 <mgambardella16@gmail.com>
2024-05-08 19:36:06 -0400

    Added multiplication app

---
+++ math001.js
@@ -0,0 +1,7 @@
+function multiplyNumber () {
+    num1 = document.getElementById("number1").value
+    num2 = document.getElementById("number2").value
+
+    document.getElementById("result").innerHTML = (num1 * num2)
+    console.log(result)
+}
