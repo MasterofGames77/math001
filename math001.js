4466c1b33ab9df94467df89fb700c85128be8c2e
MasterofGames77 <mgambardella16@gmail.com>
2024-05-08 20:01:36 -0400

    Added comments

--- math001.js
+++ math001.js
@@ -1,7 +1,14 @@
 function multiplyNumber () {
-    num1 = document.getElementById("number1").value
-    num2 = document.getElementById("number2").value
+    event.preventDefault()
+    // Pulls the number types from the HTML file
+    let num1 = document.getElementById("number1").value
+    let num2 = document.getElementById("number2").value
 
-    document.getElementById("result").innerHTML = (num1 * num2)
+    //let productResult = parseFloat(num1 * num2)
+
+    // Prints out the result 
+    document.getElementById("result").innerHTML = parseFloat(num1 * num2)
     console.log(result)
+    //document.innerHTML = `<p style="color: green"> The product of ${num1} and ${num2} is ${productResult}.</p>`
+    //console.log(productResult)
 }
