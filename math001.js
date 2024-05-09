21dce08b77e88ee9544249f1c4fba6794e0cef97
MasterofGames77 <mgambardella16@gmail.com>
2024-05-08 20:08:50 -0400

    parseFloat on variable declaration

--- math001.js
+++ math001.js
@@ -1,13 +1,13 @@
 function multiplyNumber () {
     event.preventDefault()
     // Pulls the number types from the HTML file
-    let num1 = document.getElementById("number1").value
-    let num2 = document.getElementById("number2").value
+    let num1 = parseFloat(document.getElementById("number1").value)
+    let num2 = parseFloat(document.getElementById("number2").value)
 
     //let productResult = parseFloat(num1 * num2)
 
     // Prints out the result 
-    document.getElementById("result").innerHTML = parseFloat(num1 * num2)
+    document.getElementById("result").innerHTML = num1 * num2
     console.log(result)
     //document.innerHTML = `<p style="color: green"> The product of ${num1} and ${num2} is ${productResult}.</p>`
     //console.log(productResult)
